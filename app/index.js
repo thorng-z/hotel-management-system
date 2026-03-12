22262683fcf9cd4aeb1c90d65dc0f8ae9b707504
thorng-z <lanorinthorng168@gmail.com>
2026-03-13 01:52:32 +0700

    main

--- app/index.js
+++ app/index.js
@@ -1,11 +1,29 @@
+import { useNavigation, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+    const router = useRouter()
+    useEffect(()=>{
+        const timer = setTimeout(async ()=>{
+            await SplashScreen.hideAsync();
+            router.replace('/home')
+        },5000)
+        
+        return () => clearTimeout(timer);
+    }, [])
   return (
     <View style={styles.container}>
-      <Text>Hotel Management System App!</Text>
+      <Image 
+        source={require('../assets/icons/splash-bg.png')} 
+        style={styles.backgroundImage}
+        resizeMode="cover"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +33,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
   },
 });
